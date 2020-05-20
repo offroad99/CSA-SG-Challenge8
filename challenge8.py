d4aea9dbf9bd6b3f6438abc1c60c866417344c4e
George Bekmezian <george.bekmezian@cvetech.com>
2020-05-19 18:45:32 -0600

    first commit

---
+++ challenge8.py
@@ -0,0 +1,12 @@
+import login
+from webexteamssdk import WebexTeamsAPI, ApiError
+
+myAPI = WebexTeamsAPI(login.accessToken)
+myRoom = myAPI.rooms.create("George Bekmezian-DEVNET-TEST")
+myPersonList = myAPI.people.list("george.bekmezian@cvetech.com")
+myPerson = list(myPersonList)[0]
+myAPI.memberships.create(myRoom.id, personEmail="george.bekmezian@cvetech.com")
+myAPI.messages.create(roomId=myRoom.id, text=f"Hi {myPerson.get('name')}")
+
+
+myAPI.rooms.delete(myRoom.id)
