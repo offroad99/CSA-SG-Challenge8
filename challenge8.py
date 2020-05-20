767f197266d6ef2401f0400f7e36f77738b590fc
George Bekmezian <george.bekmezian@cvetech.com>
2020-05-19 18:56:28 -0600

    fix 1

--- challenge8.py
+++ challenge8.py
@@ -6,7 +6,7 @@ myRoom = myAPI.rooms.create("George Bekmezian-DEVNET-TEST")
 myPersonList = myAPI.people.list("george.bekmezian@cvetech.com")
 myPerson = list(myPersonList)[0]
 myAPI.memberships.create(myRoom.id, personEmail="george.bekmezian@cvetech.com")
-myAPI.messages.create(roomId=myRoom.id, text=f"Hi {myPerson.get('name')}")
+myAPI.messages.create(roomId=myRoom.id, text=f"Hi {myPerson.userName}")
 
 
 myAPI.rooms.delete(myRoom.id)
